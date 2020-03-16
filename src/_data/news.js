0523dfa85fa4ec20614d6989a1d267a8e963d98b
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-03-17 00:09:14 +0100

    use country , configure redirection

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,22 @@
-
-
 require("dotenv").config()
 const axios = require("axios")
 const api_key = require("process").env["NEWS_API"]
-console.log(api_key)
-const getUrl = () => `http://newsapi.org/v2/top-headlines?country=fr&category=health&apiKey=${api_key}`
-module.exports= async function(){
-    const news  =  await axios.get(getUrl())
-    console.log(news,api_key)
+const {countries} = require("./langage.json")
+
+ 
+async function getNews (country){
+    const getUrl = (country) => `http://newsapi.org/v2/top-headlines?country=${country}&category=health&apiKey=${api_key}`
+   
     
-    return news.data
+   response =   await axios.get(getUrl(country))
+   return {
+       country,
+       articles: response.data.articles
+   }
+}
+
+module.exports = async function(){
+    
+    newsPromise = countries.map(country => getNews(country))
+       return Promise.all(newsPromise) 
 }
